803941ce37b726cdaa45e5f7a2a7815432c343ab
wvjgsuhp <wasin.pas@hotmail.com>
2024-05-06 21:44:26 +0930

    add poe layout

---
+++ keyboards/ergodox_ez/keymaps/qwerty_poe/config.h
@@ -0,0 +1,13 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#undef MOUSEKEY_WHEEL_INTERVAL
+#define MOUSEKEY_WHEEL_INTERVAL 89
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED 5
+
+#define FIRMWARE_VERSION u8"l5rlw/gvErM"
